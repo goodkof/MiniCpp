0cd64a6bebbde963de9ed7f1efe046c0a41508a3
Administrator <Administrator@PC-20170422STRX>
2017-06-10 19:02:44 +0800

    添加项目文件。

---
+++ MiniCpp/Libcpp.cpp
@@ -0,0 +1,72 @@
+// ***** Internal Library Functions ***** 
+
+// Add more of your own, here. 
+
+#include <iostream> 
+#include <cstdlib> 
+#include <cstdio> 
+#include "mccommon.h" 
+
+using namespace std;
+
+// Read a character from the console. 
+// If your compiler supplies an unbuffered 
+// character intput function, feel free to 
+// substitute it for the call to cin.get(). 
+int call_getchar()
+{
+	char ch;
+
+	ch = getchar();
+
+	// Advance past ()   
+	get_token();
+	if (*token != '(')
+		throw InterpExc(PAREN_EXPECTED);
+
+	get_token();
+	if (*token != ')')
+		throw InterpExc(PAREN_EXPECTED);
+
+	return ch;
+}
+
+// Write a character to the display. 
+int call_putchar()
+{
+	int value;
+
+	eval_exp(value);
+
+	putchar(value);
+
+	return value;
+}
+
+// Return absolute value. 
+int call_abs()
+{
+	int val;
+
+	eval_exp(val);
+
+	val = abs(val);
+
+	return val;
+}
+
+// Return a randome integer. 
+int call_rand()
+{
+
+	// Advance past ()   
+	get_token();
+	if (*token != '(')
+		throw InterpExc(PAREN_EXPECTED);
+
+	get_token();
+	if (*token != ')')
+		throw InterpExc(PAREN_EXPECTED);
+
+	return rand();
+}
